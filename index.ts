6c4a3a271e91bcad71d316fe47788064368a81d8
kimsk1 <ksangkuk@gmail.com>
2024-05-30 20:20:38 +0900

    test3

--- index.ts
+++ index.ts
@@ -3,23 +3,23 @@ import express from 'express';
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
+app.get('/', (req:any, res:any) => {
     res.send('Hi')
 })
 
-app.get('/test', (req, res) => {
+app.get('/test', (req:any, res:any) => {
     res.send('Hi Test')
 })
 
-app.get('/test2', (req, res) => {
+app.get('/test2', (req:any, res:any) => {
     res.send('Hi Test2')
 })
 
-app.get('/test3', (req, res) => {
+app.get('/test3', (req:any, res:any) => {
     res.send('Hi Test3')
 })
 
-app.get('/test4', (req, res) => {
+app.get('/test4', (req:any, res:any) => {
     res.send('Hi Test4')
 })
 
